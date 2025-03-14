389e8b4eaebc0c714e799477c43b4a71ef602a88
cchang <christoferchang18@gmail.com>
2025-03-14 12:56:17 -0400

    Add feature allowing Keyboard input to move ship to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -40,6 +41,13 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.quit:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
